1b3d3845becebca096cdd52852201e8cfabdb14b
AshrafulMorsalinSizan99 <jucse28.377@gmail.com>
2022-09-29 15:52:28 +0600

    cart created

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Activity.css'
 const Activity = (props) => {
+    const { handleAddToList } = props;
     const { img, name, age, time } = props.activity;
+
     return (
         <div className='activity'>
             <img src={img} alt="" />
-            <p className='activity-name'>{name}</p>
+            <div className='activity-info'>
+                <p className='activity-name'>{name}</p>
+                <p>For Age: {age}</p>
+                <p>Time required: {time}s</p>
+            </div>
+            <button onClick={() => props.handleAddToList(props.activity)} className='btn-cart'>
+                <p>Add to list</p>
+            </button>
         </div>
     );
 };
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import Activity from '../Activity/Activity';
 import './Work.css'
 const Work = () => {
     const [activities, setActivities] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('activities.json')
             .then(res => res.json())
             .then(data => setActivities(data))
-    }, [])
+    }, []);
+    const handleAddToList = (activity) => {
+        console.log(activity);
+        const newCart = [...cart, activity];
+        setCart(newCart);
+    }
     return (
         <div className='work-container'>
             <div className="activities-container">
@@ -15,13 +21,14 @@ const Work = () => {
                     activities.map(activity => <Activity
                         key={activity.id}
                         activity={activity}
-
+                        handleAddToList={handleAddToList}
                     >
                     </Activity>)
                 }
             </div>
             <div className="cart-container">
-                <h3>Cart Summary</h3>
+                <h3>Md. Ashraful Morsalin</h3>
+                <p>Activity time {cart.length}</p>
             </div>
         </div>
     );
